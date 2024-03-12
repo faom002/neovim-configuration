079d82487edf2a186917e6ef71efa1d9824a7643
faom002 <faisal_jobb@outlook.com>
2024-03-12 12:08:22 +0100

    added remap configuration

--- lua/faisal/remap.lua
+++ lua/faisal/remap.lua
@@ -22,6 +22,15 @@ vim.api.nvim_set_keymap('n', '<leader>j',
 
 
 
+
+
+vim.api.nvim_set_keymap('n', ';', [[:lua require('telescope.builtin').keymaps()<CR>]], { noremap = true, silent = true })
+
+
+
+
+
+
 -- Autocompleting with Ctrl + y (redo)
 vim.keymap.set("n", "<C-y>", ":redo<CR>", { noremap = true, silent = true })
 
@@ -102,22 +111,22 @@ local dap = require("dap")
 
 
 dap.adapters.codelldb = {
-  type = 'server',
-  host = '127.0.0.1',
-  port = 13000 -- 💀 Use the port printed out or specified with `--port`
+    type = 'server',
+    host = '127.0.0.1',
+    port = 13000 -- 💀 Use the port printed out or specified with `--port`
 }
 
 dap.configurations.cpp = {
-  {
-    name = "Launch file",
-    type = "codelldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-  },
+    {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+    },
 }
 
 
